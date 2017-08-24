7f6879a43fbd67adb6c273c670d0b009cccdc103
Haicheng Chen <hanseychen@gmail.com>
2017-08-23 20:00:11 -0400

    Open maximized window on start

--- src/java/org/hansey/paperdb/PaperDB.java
+++ src/java/org/hansey/paperdb/PaperDB.java
@@ -1,26 +1,34 @@
-import javafx.application.Application; 
-import javafx.collections.ObservableList; 
-import javafx.scene.Group; 
-import javafx.scene.Scene; 
-import javafx.stage.Stage; 
-import javafx.scene.text.Font; 
-import javafx.scene.text.Text; 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
+
 
 public class PaperDB extends Application { 
 	@Override 
-	public void start(Stage stage) {       
+	public void start(Stage stage) {
+		Screen screen = Screen.getPrimary();
+		Rectangle2D bounds = screen.getVisualBounds();
 
-		//Creating a Group object  
+		//Creating a Group object
 		Group root = new Group(); 
 
 		//Creating a scene object 
-		Scene scene = new Scene(root, 1200, 800); 
+		Scene scene = new Scene(root, 800, 600, Color.BLACK);
 
 		//Setting title to the Stage 
 		stage.setTitle("PaperDB"); 
 
 		//Adding scene to the stage 
-		stage.setScene(scene); 
+		stage.setScene(scene);
+
+		stage.setX(bounds.getMinX());
+		stage.setY(bounds.getMinY());
+		stage.setWidth(bounds.getWidth());
+		stage.setHeight(bounds.getHeight());
 
 		//Displaying the contents of the stage 
 		stage.show(); 
